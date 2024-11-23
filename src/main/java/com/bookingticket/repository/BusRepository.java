eb8a7f5270276c9d7ee574726c2ac81b4a60da9d
Lê Duy Khang <lehoanggiakhuong2006@gmail.com>
2024-11-23 14:35:45 +0700

    Sửa lincent

--- src/main/java/com/bookingticket/repository/BusRepository.java
+++ src/main/java/com/bookingticket/repository/BusRepository.java
@@ -2,12 +2,14 @@ package com.bookingticket.repository;
 
 import com.bookingticket.entity.Bus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface BusRepository extends JpaRepository<Bus, Long> {
-
-    Optional<Bus> findBylicense_plate(String license_Plate);
+    @Query("SELECT b FROM Bus  b WHERE b.license_plate = :license_plate")
+    Optional<Bus> findByLicensePlate(@Param("license_plate") String licensePlate);
 }
--- target/generated-sources/annotations/com/bookingticket/mapper/SeatMapperImpl.java
+++ target/generated-sources/annotations/com/bookingticket/mapper/SeatMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-23T14:01:46+0700",
+    date = "2024-11-23T14:35:28+0700",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.13 (Amazon.com Inc.)"
 )
 @Component
